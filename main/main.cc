99221696b4ea447936ee25e08e063a7f08e78c33
Gary Leutheuser <g.leutheuser@gmail.com>
2021-02-07 09:50:10 -0700

    Add progress indications

--- main/main.cc
+++ main/main.cc
@@ -9,11 +9,14 @@ int main(void) {
     std::cout << "P3\n" << image_width << " " << image_height << "\n255\n";
 
     for (int j = image_height - 1; j >= 0; --j) {
+        std::cerr << "\rScanlines remaining: " << j << " " << std::flush;
         for (int i = 0; i < image_width; ++i) {
+            // Nominal [0.0, 1.0] range RGB.
             auto r = double(i) / (image_width - 1);
             auto g = double(j) / (image_height - 1);
             auto b = 0.25;
 
+            // Scaled RGB values to fit actual output range, [0, 255].
             int ir = static_cast<int>(255.999 * r);
             int ig = static_cast<int>(255.999 * g);
             int ib = static_cast<int>(255.999 * b);
@@ -21,4 +24,6 @@ int main(void) {
             std::cout << ir << " " << ig << " " << ib << "\n";
         }
     }
+
+    std::cerr << "\nDone.\n";
 }
